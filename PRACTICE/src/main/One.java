63d6bba58509b2813e4b1db8ac327744bcb4a669
Admin <Admin@DESKTOP-D0KCSOU>
2023-06-03 11:08:12 +0530

    two

--- PRACTICE/src/main/One.java
+++ PRACTICE/src/main/One.java
@@ -2,7 +2,7 @@ package main;
 
 public class One {
 public static void main(String[] args) {
-	int no = 100;
+	int no = 1001;
 	if (no%2==0) {System.out.println("even number");
 		
 	} else {System.out.println("odd number");
