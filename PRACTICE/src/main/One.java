e72dfcc92fd5be36fa8507cc98889c80dccac0be
Admin <Admin@DESKTOP-D0KCSOU>
2023-06-03 11:05:25 +0530

    one

---
+++ PRACTICE/src/main/One.java
@@ -0,0 +1,12 @@
+package main;
+
+public class One {
+public static void main(String[] args) {
+	int no = 100;
+	if (no%2==0) {System.out.println("even number");
+		
+	} else {System.out.println("odd number");
+
+	}
+}
+}
